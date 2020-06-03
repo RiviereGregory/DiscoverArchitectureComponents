28ee8bedccb0a88f5e2e5cfdb45eb64ae9e8e009
RIVIERE <gregory.riviere@laposte.net>
2020-06-03 11:05:00 +0200

    Ajout Room#TP FOLDER création et affichage du bookmark

--- app/src/main/java/gri/riverjach/discoverarchitecturecomponents/FolderActivity.kt
+++ app/src/main/java/gri/riverjach/discoverarchitecturecomponents/FolderActivity.kt
@@ -5,6 +5,10 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.Transformations
 import kotlinx.android.synthetic.main.activity_folder.*
 import java.util.concurrent.Executors
 
@@ -14,15 +18,55 @@ fun EditText.textString(): String {
 
 class FolderActivity : AppCompatActivity() {
     private lateinit var folderDao: FolderDao
+    private lateinit var bookmarkDao: BookmarkDao
+
+    private val folderNameLiveData = MutableLiveData<String>()
+    private lateinit var getFolderLiveData: LiveData<Folder>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_folder)
 
         folderDao = App.database.folderDao()
+        bookmarkDao = App.database.bookmarkDao()
+
+        getFolderLiveData = Transformations.switchMap(folderNameLiveData) { folderName ->
+            folderDao.getFolderByName(folderName)
+        }
+
+        bookmarkDao.getAllBookmarks().observe(this, Observer { bookmarks ->
+            bookmarksTextView.text = bookmarks!!
+                .joinToString("\n") { bookmark -> bookmark.toString() }
+        })
+
+        getFolderLiveData.observe(this, Observer { folder ->
+            createBookmark(
+                folder!!.id,
+                bookmarkNameEditText.textString(),
+                bookmarkUrlEditText.textString()
+            )
+        })
 
         createFolderButton.setOnClickListener {
             createFolder(createFolderEditText.textString())
         }
+        createBookmarkButton.setOnClickListener {
+            folderNameLiveData.value = bookmarkFolderNameEditText.textString()
+        }
+    }
+
+    private fun createBookmark(folderId: Long, bookmarkName: String, bookmarkUrl: String) {
+        Executors.newSingleThreadExecutor().execute {
+            val bookmark = Bookmark(folderId = folderId, name = bookmarkName, url = bookmarkUrl)
+            bookmarkDao.insertBookmark(bookmark)
+        }
+
+        Toast.makeText(
+            this,
+            "Bookmark $bookmarkName created!",
+            Toast.LENGTH_SHORT
+        ).show()
+
     }
 
     private fun createFolder(folderName: String) {
